66d50df7893fb80d3a9b50b2c9ee0b4b297267c8
DavigJ <david.j.jung@gmail.com>
2024-11-09 22:04:42 +0400

    pehkui compat

--- src/main/java/com/davigj/blasted_barrens/core/other/BBEvents.java
+++ src/main/java/com/davigj/blasted_barrens/core/other/BBEvents.java
@@ -3,11 +3,15 @@ package com.davigj.blasted_barrens.core.other;
 import com.davigj.blasted_barrens.core.BlastedBarrens;
 import com.davigj.blasted_barrens.core.registry.BBBiomes;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Creeper;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import virtuoel.pehkui.api.ScaleTypes;
 
 @Mod.EventBusSubscriber(modid = BlastedBarrens.MOD_ID)
 public class BBEvents {
@@ -17,7 +21,26 @@ public class BBEvents {
         if (event.getLookingEntity() instanceof Creeper creeper) {
             BlockPos pos = creeper.getOnPos();
             if (creeper.level().getBiome(pos).is(BBBiomes.BLASTED_BARRENS)) {
+                if (ModList.get().isLoaded("pehkui")) {
+                    if (ScaleTypes.BASE.getScaleData(creeper).getScale() > 1.0F) {
+                        event.modifyVisibility(1.3);
+                        return;
+                    }
+                }
                 event.modifyVisibility(0.2);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void creeperVariants(EntityJoinLevelEvent event) {
+        if (!ModList.get().isLoaded("pehkui")) return;
+        if (event.getEntity() instanceof Creeper creeper) {
+            BlockPos pos = creeper.getOnPos();
+            RandomSource random = creeper.getRandom();
+            if (creeper.level().getBiome(pos).is(BBBiomes.BLASTED_BARRENS) && random.nextDouble() < 0.125) {
+                ScaleTypes.BASE.getScaleData(creeper).setTargetScale(1.0F + (0.5F * random.nextFloat()));
+                ScaleTypes.MOTION.getScaleData(creeper).setTargetScale(0.75F);
             }
         }
     }
