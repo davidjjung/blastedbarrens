cba2d29db54f11adc7394ce61e8cefbdcf8a9705
DavigJ <david.j.jung@gmail.com>
2024-12-24 11:06:16 +0400

    fixes #1, #2

--- src/main/java/com/davigj/blasted_barrens/core/data/server/tags/BBBiomeTagsProvider.java
+++ src/main/java/com/davigj/blasted_barrens/core/data/server/tags/BBBiomeTagsProvider.java
@@ -2,6 +2,7 @@ package com.davigj.blasted_barrens.core.data.server.tags;
 
 import com.davigj.blasted_barrens.core.BlastedBarrens;
 import com.davigj.blasted_barrens.core.other.tags.BBBiomeTags;
+import com.davigj.blasted_barrens.core.registry.BBBiomes;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.BiomeTagsProvider;
@@ -11,6 +12,8 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.davigj.blasted_barrens.core.other.tags.BBBiomeTags.NO_DEFAULT_MONSTERS;
+
 public class BBBiomeTagsProvider extends BiomeTagsProvider {
     public BBBiomeTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> provider, ExistingFileHelper helper) {
         super(output, provider, BlastedBarrens.MOD_ID, helper);
@@ -18,7 +21,11 @@ public class BBBiomeTagsProvider extends BiomeTagsProvider {
 
     @Override
     public void addTags(HolderLookup.Provider provider) {
-        this.tag(BBBiomeTags.HAS_SMEAR_CRATER).addTag(BiomeTags.IS_OVERWORLD);
+//        this.tag(BBBiomeTags.HAS_SMEAR_CRATER).addTag(BiomeTags.IS_OVERWORLD);
         this.tag(BBBiomeTags.HAS_PERIPHERY_CRATER).addTag(BiomeTags.IS_OVERWORLD);
+        this.tag(Tags.Biomes.IS_RARE).add(BBBiomes.BLASTED_BARRENS);
+        this.tag(Tags.Biomes.IS_DRY_OVERWORLD).add(BBBiomes.BLASTED_BARRENS);
+        this.tag(Tags.Biomes.IS_WASTELAND).add(BBBiomes.BLASTED_BARRENS);
+        this.tag(NO_DEFAULT_MONSTERS).add(BBBiomes.BLASTED_BARRENS);
     }
 }
--- src/main/java/com/davigj/blasted_barrens/core/other/tags/BBBiomeTags.java
+++ src/main/java/com/davigj/blasted_barrens/core/other/tags/BBBiomeTags.java
@@ -2,6 +2,8 @@ package com.davigj.blasted_barrens.core.other.tags;
 
 import com.davigj.blasted_barrens.core.BlastedBarrens;
 import com.teamabnormals.blueprint.core.util.TagUtil;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 
@@ -9,7 +11,13 @@ public class BBBiomeTags {
     public static final TagKey<Biome> HAS_SMEAR_CRATER = biomeTag("has_feature/has_smear_crater");
     public static final TagKey<Biome> HAS_PERIPHERY_CRATER = biomeTag("has_feature/has_periphery_crater");
 
+    public static final TagKey<Biome> NO_DEFAULT_MONSTERS = forgeTag("no_default_monsters");
+
     private static TagKey<Biome> biomeTag(String tagName) {
         return TagUtil.biomeTag(BlastedBarrens.MOD_ID, tagName);
+    }
+
+    private static TagKey<Biome> forgeTag(String name) {
+        return TagKey.create(Registries.BIOME, new ResourceLocation("forge", name));
     }
 }
